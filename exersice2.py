0cb0470c154477481488e0e8dabc4ab1fe868db0
I-like-pelmeni <maxtoo2017@gmail>
2021-10-09 18:16:15 +0300

    Add exersice2 task

--- exersice2.py
+++ exersice2.py
@@ -1,2 +1,17 @@
 class TriangleChecker:
-    def __init__(self, )
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+            self.c = c
+            
+
+    def is_triangle(self):
+        if self.a + self.b>self.c and self.c+self.b>self.a and self.a+self.c>self.b:
+            print('Ура, можно построить треугольник!')
+        elif self.a < 0 or self.b < 0  or self.c < 0:
+            print('С отрицательными числами не выйдет')
+        else:
+            print('Жаль, но из этого не сделать треугольник.')
+
+t = TriangleChecker(int(input()), int(input()), int(input()))
+t.is_triangle()
