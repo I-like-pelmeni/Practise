9936006164dab4b305c732d3e5e032a6081a5dbe
I-like-pelmeni <maxtoo2017@gmail.com>
2021-10-16 11:23:19 +0300

    Add exercise4 task

---
+++ exercise4.py
@@ -0,0 +1,42 @@
+class Alphabet:
+    def __init__(self, lang, letters):
+        self.lang = lang
+        self.letters = letters
+    
+    def print(self):
+        print(self.letters)
+
+    def letters_num(self):
+        self.letters = self.letters_num
+        return self.letters_num
+
+class EngALphabet(Alphabet):
+    def __init__(self, lang, letters):
+        self.lang = lang
+        self.letters = letters
+    
+    @staticmethod
+    def _letters_num(self):
+        self._letters_num = self.letters_num
+        return self._letters_num
+
+    def is_en_letters(self):
+        if self.letters == EngALphabet:
+            print(True)
+        else:
+            print(False)
+
+    @staticmethod
+    def _example(self):
+        print("Don't judge a book by it's cover.")
+        return self.lang
+
+def main():
+    N = str(EngALphabet)
+    N = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    print(N)
+    print()
+    print('English Example:')
+    print()
+    
+main()
