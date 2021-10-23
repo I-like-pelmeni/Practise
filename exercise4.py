37cc8b0316b4295f8f8a33ef7c4e90d58a1c8860
I-like-pelmeni <maxtoo2017@gmail.com>
2021-10-23 11:35:18 +0300

    Add exersice4 task

--- exercise4.py
+++ exercise4.py
@@ -1,42 +1,41 @@
+import string
 class Alphabet:
     def __init__(self, lang, letters):
         self.lang = lang
         self.letters = letters
     
-    def print(self):
+    def prinnt(self):
         print(self.letters)
 
+    @property
     def letters_num(self):
-        self.letters = self.letters_num
-        return self.letters_num
+        return len(self.letters)
 
 class EngALphabet(Alphabet):
-    def __init__(self, lang, letters):
-        self.lang = lang
-        self.letters = letters
+    __letters_num = 26
+
+    def __init__(self):
+        super().__init__('En', list(string.ascii_uppercase))
     
-    @staticmethod
+    @property
     def _letters_num(self):
-        self._letters_num = self.letters_num
-        return self._letters_num
+        return EngALphabet.__letters_num
 
-    def is_en_letters(self):
-        if self.letters == EngALphabet:
-            print(True)
-        else:
-            print(False)
+    def is_en_letters(self, let):
+        return let in self.letters
 
     @staticmethod
-    def _example(self):
-        print("Don't judge a book by it's cover.")
-        return self.lang
+    def example():
+        return 'Have a good lunch'
 
 def main():
-    N = str(EngALphabet)
-    N = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    print(N)
-    print()
-    print('English Example:')
-    print()
+    word = EngALphabet()
+    word.prinnt()
+    word.letters_num
+    word.is_en_letters("G")
+    word.is_en_letters("П")
+    print("English Example:")
+    EngALphabet.example()
     
-main()
+if __name__ == "__main__":
+    main()
